f52c9ea5dc3d43cf7ff15f189210c13f2daf6522
Antonio Ruggiero <antonrugg@gmail.com>
2022-04-04 17:01:08 +0200

    practice end

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ console.log('JS OK');
 const app = new Vue({
     el: '#app',
     data: {
+        emails: []
 
+        
     },
     mounted() {
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+                const result = response.data.response;
+                this.emails.push(result);
+            });
+
+            
+        }
+            
+     
         
     }
 });
